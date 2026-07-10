a8c01a99a305093b375467baea7db64189fb2ac5
Khubaib Ahmad <khubaibahmadkm@gmail.com>
2026-07-10 12:54:13 +0530

    second commit

--- package.config.ts
+++ package.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from '@sanity/pkg-utils'
 
 export default defineConfig({
   tsconfig: 'tsconfig.json',
+  dist: 'dist',
+  bundles: [
+    {
+      source: './src/index.ts',
+      import: './dist/index.js',
+      require: './dist/index.cjs',
+    },
+  ],
 })
